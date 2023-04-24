e0b89785d1356dd44d697822d90c946e72d0ce40
PetteriRauhala <122267930+PetteriRauhala@users.noreply.github.com>
2023-04-24 12:57:02 +0300

    läärää

--- game.py
+++ game.py
@@ -33,8 +33,13 @@ class Game:
         self.screen_h = self.screen.get_height()
         self.running = False
         self.font16 = pygame.font.Font("fonts/SyneMono-Regular.ttf", 16)
+        self.init_sounds()
         self.init_graphics()
         self.init_objects()
+
+    def init_sounds(self):
+        self.flying_sound = pygame.mixer.Sound("sounds/flying.wav")
+        self.hit_sound = pygame.mixer.Sound("sounds/hit.wav")
 
     def init_graphics(self):
         self.menu.set_font_size(int(48 * self.screen_h / 450))
@@ -88,10 +93,16 @@ class Game:
     def remove_oldest_obstacle(self):
         self.obstacles.pop(0)
 
-    def scale_positions(self, scale_x, scale_y):
+    def scale_positions_and_sizes(self, scale_x, scale_y):
         self.bird_pos = (self.bird_pos[0] * scale_x, self.bird_pos[1] * scale_y)
         for i in range(len(self.bg_pos)):
             self.bg_pos[i] = self.bg_pos[i] * scale_x
+        for obstacle in self.obstacles:
+            obstacle.width *= scale_x
+            obstacle.position *= scale_x
+            obstacle.upper_height *= scale_y
+            obstacle.hole_size *= scale_y
+            obstacle.lower_height *= scale_y
 
     def run(self):
         self.running = True
@@ -133,8 +144,7 @@ class Game:
                     elif event.key == pygame.K_RETURN:
                         item = self.menu.get_selected_item()
                         if item == "New Game":
-                            self.is_in_menu = False
-                            self.init_objects()
+                            self.start_game()
                         elif item == "High Scores":
                             pass  # TODO: Implement High Score view
                         elif item == "About":
@@ -144,7 +154,22 @@ class Game:
                 elif event.key in (pygame.K_SPACE, pygame.K_UP):
                     self.bird_lift = False
                 elif event.key == pygame.K_ESCAPE or not self.bird_alive:
-                    self.is_in_menu = True
+                    self.open_menu()
+
+    def start_game(self):
+        self.is_in_menu = False
+        self.init_objects()
+        self.flying_sound.play(-1)
+
+    def open_menu(self):
+        self.is_in_menu = True
+        self.flying_sound.stop()
+
+    def kill_bird(self):
+        if self.bird_alive:
+            self.bird_alive = False
+            self.flying_sound.stop()
+            self.hit_sound.play()
 
     def toggle_fullscreen(self):
         old_w = self.screen_w
@@ -159,7 +184,7 @@ class Game:
         self.screen_w = screen.get_width()
         self.screen_h = screen.get_height()
         self.init_graphics()
-        self.scale_positions(
+        self.scale_positions_and_sizes(
             scale_x=(self.screen_w / old_w),
             scale_y=(self.screen_h / old_h),
         )
@@ -197,7 +222,7 @@ class Game:
         if bird_y > self.screen_h * 0.82:
             bird_y = self.screen_h * 0.82
             self.bird_y_speed = 0
-            self.bird_alive = False
+            self.kill_bird()
 
         # Aseta linnun x-y-koordinaatit self.bird_pos-muuttujaan
         self.bird_pos = (self.bird_pos[0], bird_y)
@@ -220,7 +245,7 @@ class Game:
                 self.bird_collides_with_obstacle = True
         
         if self.bird_collides_with_obstacle:
-            self.bird_alive = False
+            self.kill_bird()
 
     def update_screen(self):
         # Täytä tausta vaaleansinisellä
